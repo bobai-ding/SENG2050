9de1bb6a438fbf32c2ce4328bb3d40d55a281997
Simi <Simi@10.222.102.126>
2019-05-17 10:54:39 +1000

    here's hoping this works

--- testCode.java
+++ testCode.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class testCode{
 
   public static void main(String[] args){
-    System.out.println("Hello World");
+    System.out.println("Hello World!");
     System.out.println("Sync Files");
     return;
   }
