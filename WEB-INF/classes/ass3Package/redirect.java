d5f86ca88993563833a8c989b705d18833aafe81
Overlord360 <simi.98@hotmail.com>
2019-06-08 01:51:13 +1000

    added sorting (nonfunctional)

--- WEB-INF/classes/ass3Package/redirect.java
+++ WEB-INF/classes/ass3Package/redirect.java
@@ -11,6 +11,8 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
 
 /**
  * Servlet implementation class redirect
@@ -30,20 +32,74 @@ public class redirect extends HttpServlet {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
 		//List<Report> reports = request.getParameter("reportList");
-		List<Report> reports = (List<Report>) request.getAttribute("reportList");
+		List<Object> reports =  (List<Object>) request.getAttribute("reportList");
 		String type = request.getParameter("order");
+		
+		Object[] arr = null;
+		
+		//System.out.println("SORTING | Reports = " + reports + " | type = " + type);
 		
 		if(reports == null) {
 			//redirect
+			request.setAttribute("user", request.getUserPrincipal());
+			request.getRequestDispatcher("/WEB-INF/jsp/user/Main.jsp").forward(request, response); //redirect to main page
+		} else {
+			arr = (Object[]) reports.toArray();
 		}
 		
+		System.out.println("SORTING | Reports = " + reports + " | type = " + type + " | arr = " + arr);
+		
 		if(type.equals("categories")) {
-			
+			String temp = "";
+			String A = "";
+			String B = "";
+			for(int i = 0; i < arr.length ;i++) {
+				for(int j = 0; j < (arr.length - 1);j++) {
+					A = ((Report) arr[j]).getType();
+					B = ((Report) arr[j+1]).getType();
+					if(A.compareTo(B) > 0) {
+						temp = A;
+						A = B;
+						B = temp;
+					}
+				}
+			}
 		} else if(type.equals("date")) {
-			
-		} else {
-			
+			LocalDate temp = null;
+			LocalDate A = null;
+			LocalDate B = null;
+			for(int i = 0; i < arr.length ;i++) {
+				for(int j = 0; j < (arr.length - 1);j++) {
+					A = ((Report) arr[j]).getDate();
+					B = ((Report) arr[j+1]).getDate();
+					if(A.compareTo(B) > 0) {
+						temp = A;
+						A = B;
+						B = temp;
+					}
+				}
+			}
+		} else { //sort by status
+			String temp = "";
+			String A = "";
+			String B = "";
+			for(int i = 0; i < arr.length ;i++) {
+				for(int j = 0; j < (arr.length - 1);j++) {
+					A = ((Report) arr[j]).getStatus();
+					B = ((Report) arr[j+1]).getStatus();
+					if(A.compareTo(B) > 0) {
+						temp = A;
+						A = B;
+						B = temp;
+					}
+				}
+			}
 		}
+		
+		reports = Arrays.asList(arr);
+		request.setAttribute("reports", reports);
+		request.getRequestDispatcher(request.getParameter("sender")).forward(request, response); //redirect to main page
+		
 	}
 
 }
--- WEB-INF/jsp/Reports.jsp
+++ WEB-INF/jsp/Reports.jsp
@@ -22,11 +22,13 @@
 	</div>
 	
 	<div class="left">
-		<form action="/redirect" method="POST">
-			Sort by: <input type="radio" name="order" value="status">By status <input type="radio" name="order" value="categories">By category <input type="radio" name="order" value="date">By date
+		<form action="redirect" method="POST">
+			Sort by: <input type="radio" name="order" value="status" required checked>By status <input type="radio" name="order" value="categories">By category <input type="radio" name="order" value="date">By date
 			<input type="submit" value="Sort!">
+			<input type="hidden" name="sender" value="/WEB-INF/jsp/Reports.jsp">
 			<!--  <input type="hidden" name="reportList" value="${reports}"> -->
-			<c:set var="reportList" value="${reports}" scope="request"/>
+			<c:set var="reportList" value="${reports}" scope="session"/>
+			
 		</form>
 	</div>
 	<h3>List of Reports</h3>
